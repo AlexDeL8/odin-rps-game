d9ef9606d6e5a42ef7704dc15b385ca764cfa116
Alex De Luca <alexnaj88@gmail.com>
2021-12-28 00:57:26 -0500

    added more style to UI with more button functions

--- script.js
+++ script.js
@@ -5,5 +5,38 @@ const rockPaperScissorMap = ['rock', 'paper', 'scissors']; //[🗻, 📃, ✂];
 function computerPlay() {
     let min = Math.ceil(0);
     let max = Math.floor(3);
-    console.log(rockPaperScissorMap[(Math.floor(Math.random() * (max - min) + min))]);
+    return rockPaperScissorMap[(Math.floor(Math.random() * (max - min) + min))];
+}
+
+function game() {
+    
+}
+
+function playRound(playerSelection, computerSelection) {    
+    if(playerSelection === computerSelection) {
+        return `${playerSelection} and ${computerSelection}, it's a TIE!`; // tie
+    }
+    switch (playerSelection) {
+        case "rock":
+            if(computerSelection === "scissor") {
+                return `${playerSelection} beats ${computerSelection}, you WIN!`;
+            } else {
+                return `${computerSelection} beats ${playerSelection}, you LOSE!`;
+            }
+        case "paper":
+            if(computerSelection === "rock") {
+                return `${playerSelection} beats ${computerSelection}, you WIN!`;
+            } else {
+                return `${computerSelection} beats ${playerSelection}, you LOSE!`;
+            }
+        case "scissor":
+            if(computerSelection === "paper") {
+                return `${playerSelection} beats ${computerSelection}, you WIN!`;
+            } else {
+                return `${computerSelection} beats ${playerSelection}, you LOSE!`;
+            }
+        default:
+            console.error("playerSelection was not a valid selection!");
+            break;
+    }
 }
